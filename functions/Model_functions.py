c1e335ce6a2123aa2419d67e7b54f2bfa0b66a64
NarutoUchiha39 <anirudhata123@gmail.com>
2024-12-24 16:11:45 +0530

    Added classification notebook

---
+++ functions/Model_functions.py
@@ -0,0 +1,207 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision
+from torchvision import models,transforms
+from PIL import Image
+import cv2
+
+def train_epoch(model, train_loader, criterion, optimizer, device):
+    model.train()
+    train_loss = 0.0
+    correct = 0
+    total = 0
+
+    for data, labels in train_loader:
+        data, labels = data.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(data)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    avg_train_loss = train_loss / len(train_loader)
+    train_accuracy = 100 * correct / total
+    return avg_train_loss, train_accuracy
+
+def validate_epoch(model, val_loader, criterion, device):
+    model.eval()
+    valid_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data, labels in val_loader:
+            data, labels = data.to(device), labels.to(device)
+            outputs = model(data)
+            loss = criterion(outputs, labels)
+
+            valid_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    avg_valid_loss = valid_loss / len(val_loader)
+    valid_accuracy = 100 * correct / total
+    return avg_valid_loss, valid_accuracy
+
+def test_model(model, test_loader, device):
+    model.eval()
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data, labels in test_loader:
+            data, labels = data.to(device), labels.to(device)
+            outputs = model(data)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    test_accuracy = 100 * correct / total
+    return test_accuracy
+
+def train_and_validate_model(model, train_loader, val_loader, test_loader, criterion, patience, optimizer, device, epochs, model_filename, verbose=False):
+    min_valid_loss = np.inf
+
+    total_loss_train = []
+    total_loss_val = []
+    total_acc_train = []
+    total_acc_val = []
+    stopping_ct = 0
+
+    for epoch in tqdm(range(epochs)):
+        if stopping_ct < patience:            
+            train_loss, train_acc = train_epoch(model, train_loader, criterion, optimizer, device)
+            valid_loss, valid_acc = validate_epoch(model, val_loader, criterion, device)
+
+            total_loss_train.append(train_loss)
+            total_loss_val.append(valid_loss)
+            total_acc_train.append(train_acc)
+            total_acc_val.append(valid_acc)
+            if verbose:
+                print(f'Epoch {epoch+1}')
+                print(f'Training Loss: {train_loss} \nTraining Accuracy: {train_acc}%')
+                print(f'Validation Loss: {valid_loss} \nValidation Accuracy: {valid_acc}%')
+                print('=======================================================================')
+        if min_valid_loss > valid_loss:
+            print(f'Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f}) \nSaving The Model')
+            print('=======================================================================')
+            min_valid_loss = valid_loss
+            torch.save(model.state_dict(), model_filename)
+            stopping_ct = 0
+        else:
+            stopping_ct += 1
+
+    best_model_state = torch.load(model_filename)
+    model.load_state_dict(best_model_state)
+    return model,total_loss_train, total_loss_val, total_acc_train, total_acc_val
+
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def initialise_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    '''
+    grad = if the pretrained weights be updated
+    use_pretrained = use pre trained weights or start from scratch
+    '''
+    model = None
+    
+    if model_name == 'resnet_pret':
+        model_ft = models.resnet50(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+    elif model_name == 'densenet_pret':
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+    else:
+        print("Invalid model name, choose between 'resnet_pret', 'densenet_pret'.")
+        exit()
+    return model_ft
+
+
+def conf_report(model_fitted,loader, model_type):
+    """
+    Generate and display a confusion matrix and a classification report for the predictions made by a model.
+
+    Parameters:
+    - model_fitted: The trained model that will be used to make predictions. The model should be compatible
+                    with the data provided by the loader and should have a `to` method for device management
+                    and an `argmax` method for getting predictions.
+    - loader: A data loader that provides batches of data. Each batch should be a tuple containing data
+              and its corresponding labels. The loader should be iterable.
+    - model_type: A string indicating the type of the model. This is used for labeling purposes
+                  when saving the confusion matrix figure.
+
+    The function does the following:
+    - Iterates over the provided data loader to collect model predictions and true labels.
+    - Computes a confusion matrix both in raw and normalized form.
+    - Displays heatmaps for both raw and normalized confusion matrices.
+    - Prints a classification report showing main classification metrics.
+
+    Outputs:
+    - A figure with two subplots: the left one showing the normalized confusion matrix and the right
+      one showing the raw confusion matrix. The figure is saved as 'Confusion_Matrix_{model_type}.png'.
+    - A printed classification report in the console.
+    """
+    y_pred = []
+    y_true = []
+    for data, target in loader:
+        data, target = data.to('cpu'), target.to('cpu')
+        model_cpu = model_fitted.to('cpu')
+        output = model_cpu(data)
+        y_pred.extend(output.argmax(dim=1).tolist())
+        y_true.extend(target.tolist())
+    
+    classes = [ 'bkl','nv','df','mel','vasc','bcc','akiec']
+
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    df_temp = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1)[:, np.newaxis], index=classes, columns=classes)
+
+    plt.figure(figsize=(24, 10))  
+
+    plt.subplot(1, 2, 1)  
+    sns.heatmap(df_temp, annot=True, fmt=".3f", cmap='Blues', xticklabels=classes, yticklabels=classes)
+    plt.title('Normalized Confusion Matrix')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+
+    plt.subplot(1, 2, 2)  
+    sns.heatmap(cf_matrix, annot=True, fmt='g', cmap='Blues', xticklabels=classes, yticklabels=classes)
+    plt.title('Raw Confusion Matrix')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+
+    plt.savefig(f'Confusion_Matrix_{model_type}.png')
+    plt.show()
+
+    report = classification_report(y_true, y_pred, target_names=classes)
+    print('-------------------------------------------------------')
+    print("Classification Report:\n")
+    print(report)
+    print('-------------------------------------------------------')
---
+++ functions/__init__ .py
@@ -0,0 +1,3 @@
+from .functions import  Model_functions, normalization, classic_ml
+
+__all__ = ['Model_functions', 'normalization', 'classic_ml']
